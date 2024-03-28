7350b31332d4b1da7f358aa4b939d3940c41fca6
chakinam srinithya <161116629+Chakinamnithya@users.noreply.github.com>
2024-03-28 12:39:21 +0530

    Add files via upload

---
+++ studentImpl.java
@@ -0,0 +1,17 @@
+public class studentImpl{
+    public static void main(String[] args){
+       student s=new student("james","7081");
+         s.display();
+     }
+}
+class student{
+String nam,rn;
+student(String name,String rno){
+nam=name;
+rn=rno;
+}
+public void display(){
+System.out.printf("welcome %s rno is %s\n",nam,rn);
+}
+}
+ 
