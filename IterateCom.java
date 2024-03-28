346154387eac7498d975c0c1976492a796329de1
Chakinamnithya <161116629+Chakinamnithya@users.noreply.github.com>
2024-03-28 12:04:48 +0530

    Add files via upload

---
+++ IterateCom.java
@@ -0,0 +1,17 @@
+class IterateCom{
+   public static void main(String[] ar){
+             int[] arr={1,2,4,5,6,7};
+              ComArray co=new ComArray();
+             System.out.println("array 3 elements:");
+              for(int l:co.arr3){
+                      System.out.println(l);
+                     }
+
+               ComArray.main(null);
+               System.out.println("elements are");
+               for( int i=0;i<arr.length;i++){
+
+               System.out.println(arr[i]);
+}
+}
+}
