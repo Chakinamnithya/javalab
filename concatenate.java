12662075b92d158cc00c1d0a3358196b2eac9510
chakinam srinithya <161116629+Chakinamnithya@users.noreply.github.com>
2024-03-30 14:33:47 +0530

    Add files via upload

---
+++ concatenate.java
@@ -0,0 +1,13 @@
+class concatenate{
+           public static void main(String[] args)
+{
+           String first="java";
+           String second="lab";
+           String complete=first.concat(second);
+           System.out.println(complete);
+           String names="james";
+           String lname="gosling";
+           String auth=names+lname;
+           System.out.println(auth);
+}
+}
