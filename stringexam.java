a7bfa6859c8ea16efdfeea994238f6086de23917
chakinam srinithya <161116629+Chakinamnithya@users.noreply.github.com>
2024-03-30 14:53:46 +0530

    Add files via upload

---
+++ stringexam.java
@@ -0,0 +1,10 @@
+class stringexam{
+   public static void main(String[] args){
+    String str="tonight";
+    System.out.println(str.indexOf('u'));
+    System.out.println(str.indexOf('t',3));
+     String substring="ton";
+System.out.println(str.indexOf(substring));
+System.out.println(str.indexOf(substring,7));
+}
+}
