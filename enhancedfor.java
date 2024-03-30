65733c3888fa3f36678da312eba2686e538539d8
chakinam srinithya <161116629+Chakinamnithya@users.noreply.github.com>
2024-03-30 15:19:35 +0530

    Add files via upload

---
+++ enhancedfor.java
@@ -0,0 +1,8 @@
+class enhancedfor{
+       public static void main(String[] args){
+       int[] marks={10,20,30,40,50};
+         for(int i:marks){
+                  System.out.println("marks are"+i);
+             }
+}
+}
